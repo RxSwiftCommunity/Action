3d687a542abfe222a1fd56900c541c2d082dfa8b
Cezary Kopacz <salvation.sv@gmail.com>
2016-05-06 16:49:37 +0200

    Added carthage support

---
+++ Action/Action/Action.h
@@ -0,0 +1,19 @@
+//
+//  Action.h
+//  Action
+//
+//  Created by Cezary Kopacz on 06.05.2016.
+//  Copyright © 2016 CezaryKopacz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Action.
+FOUNDATION_EXPORT double ActionVersionNumber;
+
+//! Project version string for Action.
+FOUNDATION_EXPORT const unsigned char ActionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Action/PublicHeader.h>
+
+
