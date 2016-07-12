9e5d563d9d9bd690f7b4d632070627a63c95125f
Yosuke Ishikawa <y@ishkawa.org>
2016-07-13 01:17:17 +0900

    Set up test target

---
+++ Tests/ActionTests.swift
@@ -0,0 +1,13 @@
+//
+//  ActionTests.swift
+//  Tests
+//
+//  Created by Yosuke Ishikawa on 2016/07/13.
+//  Copyright © 2016年 CezaryKopacz. All rights reserved.
+//
+
+import XCTest
+
+class ActionTests: XCTestCase {
+    
+}
