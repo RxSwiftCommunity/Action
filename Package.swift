48d107bcc310fc8a838fa55b07fb151dbc699dc7
Yosuke Ishikawa <y@ishkawa.org>
2016-11-21 11:11:43 +0900

    Use #if os instead of excluding source files

--- Package.swift
+++ Package.swift
@@ -5,11 +5,6 @@ let package = Package(
     targets: [],
     dependencies: [
         .Package(url: "https://github.com/ReactiveX/RxSwift.git", majorVersion: 3)
-    ],
-    exclude: [
-        "Sources/Action/UIKitExtensions/AlertAction.swift",
-        "Sources/Action/UIKitExtensions/UIBarButtonItem+Action.swift",
-        "Sources/Action/UIKitExtensions/UIButton+Rx.swift",
     ]
 )
 
--- Sources/Action/UIKitExtensions/UIBarButtonItem+Action.swift
+++ Sources/Action/UIKitExtensions/UIBarButtonItem+Action.swift
@@ -1,3 +1,4 @@
+#if os(iOS) || os(tvOS)
 import UIKit
 import RxSwift
 import RxCocoa
@@ -37,3 +38,4 @@ public extension Reactive where Base: UIBarButtonItem {
         }
     }
 }
+#endif
