2d7ef8aa18402de9e0ec549ece662dd19d7f1d0a
Econa77 <s.f.1992.ip@gmail.com>
2017-05-25 15:29:11 +0900

    Replace PublishSubject to InputSubject

--- Sources/Action/Action.swift
+++ Sources/Action/Action.swift
@@ -27,7 +27,7 @@ public final class Action<Input, Element> {
     /// This subject also includes inputs as aguments of execute().
     /// All inputs are always appear in this subject even if the action is not enabled.
     /// Thus, inputs count equals elements count + errors count.
-    public let inputs = PublishSubject<Input>()
+    public let inputs = InputSubject<Input>()
 
     /// Errors aggrevated from invocations of execute().
     /// Delivered on whatever scheduler they were sent from.
