ec84fadc61f62a8d463da230dc5958722aeb1411
mosamer <mostafamamer@gmail.com>
2017-11-21 22:46:35 +0100

    rename `enabled` ~> `isEnabled`

--- Tests/ActionTests/ActionTests.swift
+++ Tests/ActionTests/ActionTests.swift
@@ -75,7 +75,7 @@ class ActionTests: QuickSpec {
 					.bind(to: errors)
 					.disposed(by: disposeBag)
 				
-				action.enabled
+				action.isEnabled
 					.bind(to: enabled)
 					.disposed(by: disposeBag)
 				
@@ -95,7 +95,7 @@ class ActionTests: QuickSpec {
 				action.inputs.subscribe().disposed(by: disposeBag)
 				action.elements.subscribe().disposed(by: disposeBag)
 				action.errors.subscribe().disposed(by: disposeBag)
-				action.enabled.subscribe().disposed(by: disposeBag)
+				action.isEnabled.subscribe().disposed(by: disposeBag)
                 action.isExecuting.subscribe().disposed(by: disposeBag)
 				action.executionObservables.subscribe().disposed(by: disposeBag)
 			}
