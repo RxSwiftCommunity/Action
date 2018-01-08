79c9e0fa2c941cb54c0d17abbbaac41bf4a803a0
yysskk <yusuke.0213@gmail.com>
2018-01-08 23:29:34 +0900

    Update RxSwift to 4.1.0

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ let package = Package(
     name: "Action",
     targets: [],
     dependencies: [
-        .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(4, 0, 0, prereleaseIdentifiers: ["alpha.1"]))
+        .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(4, 1, 0))
     ],
     exclude: ["Tests/"]
 )
