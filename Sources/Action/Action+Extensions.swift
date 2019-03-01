4657e4d9acbb8f81b847e307834f0ed885db6e30
bobgodwinx <bobgodwinx@gmail.com>
2019-03-01 15:52:54 +0100

    Initial introduction of `underlyingError` extension to output `Swift.Error`

--- Sources/Action/Action+Extensions.swift
+++ Sources/Action/Action+Extensions.swift
@@ -7,3 +7,17 @@
 //
 
 import Foundation
+import RxSwift
+
+extension Action {
+    /// Filters out `notEnabled` errors and returns
+    /// only underlying error from `ActionError`
+    var underlyingError: Observable<Error?> {
+        return errors.map { actionError in
+            guard case .underlyingError(let error) = actionError else {
+                return nil
+            }
+            return error
+        }
+    }
+}
