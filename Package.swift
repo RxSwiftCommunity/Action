2a757a82d96c3c894ff1891d96923de556ebe588
Luciano Almeida <passos.luciano@outlook.com>
2019-04-12 09:18:57 -0300

    Updating spm syntax to new swift-tools (#192)
    * Upating dependencies to Rx 4.5.0
    
    * Reverting version.
    
    * From 4.0.0 on package
    
    * Apply suggestions from code review

--- Package.swift
+++ Package.swift
@@ -1,11 +1,25 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
     name: "Action",
-    targets: [],
+    products: [
+        // Products define the executables and libraries produced by a package, and make them visible to other packages.
+        .library(
+            name: "Action",
+            targets: ["Action"]),
+        ],
     dependencies: [
-        .Package(url: "https://github.com/ReactiveX/RxSwift.git", Version(4, 0, 0, prereleaseIdentifiers: ["alpha.1"]))
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "4.0.0")
     ],
-    exclude: ["Tests/"]
+    targets: [
+        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+        .target(
+            name: "Action",
+            dependencies: ["RxSwift", "RxCocoa"],
+            path: "Sources/Action")
+    ]
 )
 
