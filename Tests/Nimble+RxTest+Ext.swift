87305644a1b382b9cff447d432a7bdf67cc680d2
bobgodwinx <bobgodwinx@gmail.com>
2019-05-08 18:28:57 +0200

    Specs-Cleanup: Introduced `func asString() -> String` to get the describing strings for `Optional` Types

--- Tests/Nimble+RxTest+Ext.swift
+++ Tests/Nimble+RxTest+Ext.swift
@@ -1,1 +1,14 @@
 import Foundation
+import Quick
+import Nimble
+import RxSwift
+import RxTest
+
+/// `Foundation`
+
+extension Optional {
+    func asString() -> String {
+        guard let s = self else { return "nil" }
+        return String(describing: s)
+    }
+}
