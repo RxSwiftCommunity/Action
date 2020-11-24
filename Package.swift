02b1a2630fd118bfaa8f692c70723e6d5e2f41d8
François Boulais <francois@appcraftstudio.com>
2020-11-24 17:07:12 +0100

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
             targets: ["Action"]),
         ],
     dependencies: [
-        .package(url: "https://github.com/appcraftstudio/Action.git", "5.0.0" ..< "7.0.0"),
+        .package(url: "https://github.com/ReactiveX/RxSwift.git", "5.0.0" ..< "7.0.0"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
